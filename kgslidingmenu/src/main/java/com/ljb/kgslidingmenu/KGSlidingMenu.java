87e1a77155282fd0d179283e13611a6257fe8b11
ljb <837187695@qq.com>
2018-09-20 22:41:42 +0800

    更新仿酷狗侧滑菜单代码

--- kgslidingmenu/src/main/java/com/ljb/kgslidingmenu/KGSlidingMenu.java
+++ kgslidingmenu/src/main/java/com/ljb/kgslidingmenu/KGSlidingMenu.java
@@ -17,38 +17,44 @@ import android.widget.HorizontalScrollView;
  * Author      :ljb
  * Date        :2018/6/3
  * Description : 仿酷狗侧滑菜单
+ *
+ * 1. 对指定内容宽度（屏幕宽度）和内容宽度
+ 2. 默认是关闭的，手指抬起判断是打开状态还是关闭状态
+ 3. 处理快速滑动
+ 4. 处理内容缩放，菜单部分有位移和透明度
+ 5. 事件分发
  */
 public class KGSlidingMenu extends HorizontalScrollView {
-    private float mMenuRightMargin;
-    /*菜单宽度*/
+
     private int mMenuWidth;
-    /*主内容View*/
-    private View mContentView;
-    /*菜单View*/
     private View mMenuView;
-    /*菜单是否打开*/
-    private boolean mMenuIsOpen;
-    /*手势处理*/
+    private View mContentView;
     private GestureDetector mGestureDetector;
-    /*是否拦截*/
-    private boolean mIsIntercept;
-    /**
-     * 解决快速滑动切换
-     */
-    private GestureDetector.OnGestureListener mListener = new GestureDetector.SimpleOnGestureListener() {
+    //菜单是否打开
+    private boolean mMenuIsOpen = false;
+    // 是否拦截onTouchEvent()里面的代码
+    private boolean mIsInterceptTouchEvent=false;
+
+    public KgSlideView(Context context) {
+        this(context, null);
+    }
+
+    public KgSlideView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    private GestureDetector.OnGestureListener mOnGestureListener = new GestureDetector.SimpleOnGestureListener() {
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            // 快速滑动，打开/关闭菜单
-            // 条件：（相对于主菜单的打开和关闭    ）打开的时候往左边快速滑动切换（关闭），关闭的时候往右快速滑动切换（打开）
+            // 快速滑动处理打开/关闭菜单
+            // 左滑 velocityX<0 右滑 velocityX>0
             if (mMenuIsOpen) {
-                //打开的时候往左边快速滑动切换（关闭）
-                if (velocityX < 0 && Math.abs(velocityX) > Math.abs(velocityY)) {
+                if (velocityX < 0 && Math.abs(velocityX)>Math.abs(velocityY)) {// 关闭
                     closeMenu();
                     return true;
                 }
             } else {
-                //关闭的时候往右快速滑动切换
-                if (velocityX > 0&& Math.abs(velocityX) > Math.abs(velocityY)) {
+                if (velocityX > 0&& Math.abs(velocityX)>Math.abs(velocityY)) {//打开
                     openMenu();
                     return true;
                 }
@@ -57,96 +63,106 @@ public class KGSlidingMenu extends HorizontalScrollView {
         }
     };
 
-    public KGSlidingMenu(Context context) {
-        this(context, null);
-    }
-
-    public KGSlidingMenu(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public KGSlidingMenu(Context context, AttributeSet attrs, int defStyleAttr) {
+    public KgSlideView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        obtainAttrs(context, attrs);
-        // 计算菜单页的宽度
-        mMenuWidth = (int) (getScreenWith() - mMenuRightMargin);
-        mGestureDetector = new GestureDetector(getContext(), mListener);
+        // 获取自定义属性
+        initAttr(context, attrs);
+        setOverScrollMode(OVER_SCROLL_NEVER);
+        mGestureDetector = new GestureDetector(context, mOnGestureListener);
     }
 
-    private void obtainAttrs(Context context, AttributeSet attrs) {
+    private void initAttr(Context context, AttributeSet attrs) {
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.KGSlidingMenu);
-        mMenuRightMargin = ta.getDimension(R.styleable.KGSlidingMenu_menuRightMargin, dp2px(50));
-        ta.recycle();
+        float rightMargin = 0;
+        if (ta != null) {
+            rightMargin = ta.getDimension(R.styleable.KGSlidingMenu_menuRightMargin, 0);
+            ta.recycle();
+        }
+        mMenuWidth = (int) (getScreentWidth(context) - rightMargin);
     }
 
     @Override
     protected void onFinishInflate() {
-        // 这个方法是布局解析完毕才会回调，也就是XML解析完毕
         super.onFinishInflate();
-        // 也就是LinearLayout
-        ViewGroup container = (ViewGroup) getChildAt(0);
-        int childCount = container.getChildCount();
+        // 包裹View
+        ViewGroup contentParent = (ViewGroup) getChildAt(0);
+        int childCount = contentParent.getChildCount();
         if (childCount != 2) {
             throw new RuntimeException("只能放2子View");
         }
-        // 1. 菜单页是屏幕的宽度
-        mMenuView = container.getChildAt(0);
-        ViewGroup.LayoutParams menuParam = mMenuView.getLayoutParams();
-        menuParam.width = mMenuWidth;
-        mMenuView.setLayoutParams(menuParam);
-        // 2. 内容页的宽度是屏幕的宽度 - 右边一小部分宽度（自定义属性）
-        mContentView = container.getChildAt(1);
-        ViewGroup.LayoutParams contentParam = mContentView.getLayoutParams();
-        contentParam.width = getScreenWith();
-        mContentView.setLayoutParams(contentParam);
+        // 菜单View
+        mMenuView = contentParent.getChildAt(0);
+        // 设置菜单宽度
+        ViewGroup.LayoutParams lp1 = mMenuView.getLayoutParams();
+        lp1.width = mMenuWidth;
+        //
+        mMenuView.setLayoutParams(lp1);
+
+        // 内容View
+        mContentView = contentParent.getChildAt(1);
+        // 设置内容View的宽度
+        ViewGroup.LayoutParams lp2 = mContentView.getLayoutParams();
+        lp2.width = (int) getScreentWidth(getContext());
+        mContentView.setLayoutParams(lp2);
     }
 
-    /**
-     * 手指抬起，要么关闭，要么打开
-     *
-     * @param ev
-     * @return
-     */
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        // 布局摆放完毕后，再调用scrollTo()方法才能起到作用（默认关闭菜单）
+        scrollTo(mMenuWidth, 0);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        // 菜单打开时，触摸右边部分，事件不会分发到子View，并且关闭菜单
+        mIsInterceptTouchEvent=false;
+        if(mMenuIsOpen){
+            float currentX =ev.getX();
+            if(currentX>mMenuWidth){
+                // 1. 关闭菜单
+                closeMenu();
+                // 3. 因为返回true拦截了事件，所以会回调onTouchEvent()，所以得搞个开关控制
+                // 能不能调用onTouchEvent()里面的代码，否则上面的colseMenu()会有问题
+                mIsInterceptTouchEvent=true;
+                // 2. 返回true拦截事件，事件不会分发到子View
+                return true;
+            }
+        }
+        return super.onInterceptTouchEvent(ev);
+
+    }
+
+    // 处理手指抬起逻辑，打开菜单/关闭菜单，通过滚动位置进行判断
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        if (mIsIntercept) return true;
-        if (mGestureDetector.onTouchEvent(ev)) {// 快速滑动触发了，就不要继续下面的判断
+        // 拦截onTouchEvent后续代码
+        if(mIsInterceptTouchEvent) return true;
+        if (mGestureDetector.onTouchEvent(ev)) {
+            // 如果mGestureDetecotr已经处理了滑动事件，事件就没必要继续
+            // 往下传递，否则在onFling()中的实现的效果，会被后面的方法
+            // 覆盖掉
             return true;
         }
         if (ev.getAction() == MotionEvent.ACTION_UP) {
-            // 根据当前滚动的距离来判断
             int currentScrollX = getScrollX();
-            if (currentScrollX > mMenuWidth / 2) { // 关闭
+            if (currentScrollX > mMenuWidth / 2) {//关闭菜单
                 closeMenu();
-
-            } else {  // 打开
+            } else {//打开菜单
                 openMenu();
             }
-            // UP事件返回true，不交非HorizontalScrollView处理，因为它会覆盖我的操作，达不到我们要的效果
+            // 确保 super.onTouchEvent()不会执行
+            // 如果不这么处理，smoothScrollTo()方法里调用的 mScroller.startScroll()会被
+            // super.onTouchEvent(）内调用的  mScroller.fling()覆盖
             return true;
         }
         return super.onTouchEvent(ev);
     }
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        mIsIntercept = false;
-        // 菜单打开时，点击内容页需要拦截事件
-        if (mMenuIsOpen) {
-            float currentX = ev.getX();
-            if (currentX > mMenuWidth) {
-                // 关闭菜单
-                closeMenu();
-                mIsIntercept = true;
-                // 拦截事件，但是会相应onTouchEvent()事件
-                return true;
-            }
-        }
-        return super.onInterceptTouchEvent(ev);
-    }
-
     /**
-     * 处理右边的缩放，左边的缩放和透明度，需要不断的获取当前滚动的位置；
+     * 滑动过程中，需要实现的效果：
+     * 1. 右边缩放
+     * 2. 左边缩放和透明度变化
      *
      * @param l
      * @param t
@@ -156,31 +172,33 @@ public class KGSlidingMenu extends HorizontalScrollView {
     @Override
     protected void onScrollChanged(int l, int t, int oldl, int oldt) {
         super.onScrollChanged(l, t, oldl, oldt);
-        // 算一个梯度值
-        float scale = 1f * l / mMenuWidth; // 变化 1-0
-        // 右边缩放：最小是0.7 最大是 1f
+        // l 的变化范围 0 - mMenuWidth
+        // 1. 设置右边缩放
+        // 1.1 计算一个梯度值
+        float scale = 1f * l / mMenuWidth;
+        // 1.2 右边缩放变化范围：0.7f - 1f
         float rightScale = 0.7f + 0.3f * scale;
-        // 设置右边的缩放
-        // 设置缩放的中心点位置
+        // 1.3 进行缩放 PS:如果不对缩放中心点进行设置，默认是View的中心位置
         ViewCompat.setPivotX(mContentView, 0);
         ViewCompat.setPivotY(mContentView, mContentView.getMeasuredHeight() / 2);
-        ViewCompat.setScaleY(mContentView, rightScale);
         ViewCompat.setScaleX(mContentView, rightScale);
-        // 设置左边透明度和缩放
-        // 透明度：半透明到完全透明 0.7f -1.0 f
-        float leftAlpha = 0.5f + (1 - scale) * 0.5f;
-        ViewCompat.setAlpha(mMenuView, leftAlpha);
-        // 缩放： 0.7f - 1.0f
+        ViewCompat.setScaleY(mContentView, rightScale);
+
+        // 2. 设置左边菜单缩放和透明度
+        // 2.1 设置透明度
+        float alpha = 0.5f + (1 - scale) * 0.5f;
+        ViewCompat.setAlpha(mMenuView, alpha);
+        // 2.2 设置缩放
         float leftScale = 0.7f + (1 - scale) * 0.3f;
-        ViewCompat.setScaleY(mMenuView, leftScale);
         ViewCompat.setScaleX(mMenuView, leftScale);
-        // 最后一个效果，抽屉效果 l * 0.25f
-        ViewCompat.setTranslationX(mMenuView, 0.25f * l);
+        ViewCompat.setScaleY(mMenuView, leftScale);
+        // 2.3 用平移，实现抽屉效果
+        ViewCompat.setTranslationX(mMenuView, l * 0.25f);
 
     }
 
     /**
-     * 打开菜单，滚动到0
+     * 打开菜单，滚动到（0,0）
      */
     private void openMenu() {
         smoothScrollTo(0, 0);
@@ -188,37 +206,21 @@ public class KGSlidingMenu extends HorizontalScrollView {
     }
 
     /**
-     * 关闭菜单， 滚动到 mMenuWidth
+     * 关闭菜单，滚动到 （mMenuWidth，0）
      */
     private void closeMenu() {
         smoothScrollTo(mMenuWidth, 0);
         mMenuIsOpen = false;
     }
 
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        super.onLayout(changed, l, t, r, b);
-        // 初始化进来是关闭的
-        scrollTo(mMenuWidth, 0);
-    }
-
     /**
-     * dp -- > px
+     * 获取屏幕宽度
      *
-     * @param value
+     * @param context
      * @return
      */
-    private int dp2px(int value) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, getResources().getDisplayMetrics());
-    }
-
-    /**
-     * 获取屏幕的宽度
-     *
-     * @return
-     */
-    private int getScreenWith() {
-        return getResources().getDisplayMetrics().widthPixels;
+    private float getScreentWidth(Context context) {
+        return context.getResources().getDisplayMetrics().widthPixels;
     }
 
 }
